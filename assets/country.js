07bfa25f2248f7a8905178a701779b31e75f76a9
Harshpalsinh  R Jadeja <114898124+harshpalsinh05@users.noreply.github.com>
2024-02-20 23:32:44 +0530

    Update country.js

--- assets/country.js
+++ assets/country.js
@@ -1,7 +1,52 @@
 const countryname = new URLSearchParams(location.search).get('name')
+const flagImage = document.querySelector('.country-details img')
+const countryNameH1 = document.querySelector('.country-details h1')
+const nativeName = document.querySelector('.native-name')
+const population = document.querySelector('.population')
+const region = document.querySelector('.region')
+const subregion = document.querySelector('.sub-region')
+const capital = document.querySelector('.capital')
+const topLevelDomain = document.querySelector('.top-level-domain')
+const currencies = document.querySelector('.currencies')
+const languages = document.querySelector('.languages')
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryname}?fullText=true`)
-.then((res) => res.json())
-.then((data)=> {
-    console.log(data[0])
-})
+    .then((res) => res.json())
+    .then(([country]) => {
+        console.log(country)
+
+        flagImage.src = country.flags.svg
+        countryNameH1.innerHTML = country.name.common
+
+        if (country.name.nativeName) {
+            nativeName.innerHTML = Object.values(country.name.nativeName)[0].common
+        }
+        else {
+            nativeName.innerHTML = country.name.common
+        }
+
+        population.innerHTML = country.population.toLocaleString('en-IN')
+        region.innerHTML = country.region
+
+        if (country.subregion) {
+            subregion.innerHTML = country.subregion
+        }
+
+        if(country.capital){
+            capital.innerHTML = country.capital?.[0]
+        }
+        
+        topLevelDomain.innerHTML = country.tld.join(', ')
+
+        if (country.currencies) {
+            // currencies.innerHTML = Object.values(country.currencies).map((currencies) => currencirs.symbol)
+            currencies.innerHTML = Object.values(country.currencies).map((currencies) => currencies.name).join(', ')
+        }
+
+        if (country.languages) {
+            languages.innerHTML = Object.values(country.languages).join(', ')
+        }
+
+
+    })  
